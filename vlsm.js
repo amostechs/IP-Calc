0445679961745a3c63850dcb462151cfa2a33127
Amos Inoti <amost3ch@gmail.com>
2026-04-20 15:25:10 +0300

    Add VLSM calculation function

--- vlsm.js
+++ vlsm.js
@@ -1,1 +1,29 @@
+export function calculateVLSM(base, hostsStr) {
+  const ipToInt = ip => ip.split('.').reduce((a,o)=>(a<<8)+ +o,0)>>>0;
+  const intToIp = int => [(int>>>24),(int>>16&255),(int>>8&255),(int&255)].join('.');
+
+  let [ip] = base.split('/');
+  let current = ipToInt(ip);
+
+  const hosts = hostsStr.split(',').map(x=>parseInt(x.trim())).sort((a,b)=>b-a);
+
+  let output = '';
+
+  hosts.forEach(h => {
+    let needed = Math.ceil(Math.log2(h+2));
+    let size = 32-needed;
+
+    let net = current;
+    let broad = net + (2**needed)-1;
+
+    output += `Subnet ${h} hosts:<br>
+    ${intToIp(net)}/${size}<br>
+    Broadcast: ${intToIp(broad)}<br><br>`;
+
+    current = broad + 1;
+  });
+
+  return output;
+}
+
 
